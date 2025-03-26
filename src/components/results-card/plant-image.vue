3aaae940669214e72c6fbdafc8627d7775e37919
Isabella Schepisi <schepisii2@gmail.com>
2025-03-26 18:56:20 -0500

    Update width for all plant images

--- src/components/results-card/plant-image.vue
+++ src/components/results-card/plant-image.vue
@@ -7,10 +7,12 @@ const props = defineProps(['plant']);
 <template>
 	<img
 		v-if="props.plant.type === PlantType.Fern"
+		style="width: 300px"
 		src="../../imgs/plant/fern.jpg"
 	/>
 	<img
 		v-if="props.plant.type === PlantType.Pothos"
+		style="width: 300px"
 		src="../../imgs/plant/pothos.jpg"
 	/>
 	<img
@@ -20,14 +22,17 @@ const props = defineProps(['plant']);
 	/>
 	<img
 		v-if="props.plant.type === PlantType.SnakePlant"
+		style="width: 300px"
 		src="../../imgs/plant/snake-plant.jpg"
 	/>
 	<img
 		v-if="props.plant.type === PlantType.SpiderPlant"
+		style="width: 300px"
 		src="../../imgs/plant/spider-plant.jpg"
 	/>
 	<img
 		v-if="props.plant.type === PlantType.ZZPlant"
+		style="width: 300px"
 		src="../../imgs/plant/zz-plant.jpg"
 	/>
 </template>
