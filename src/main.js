9bf7670dc6f706fd40edbcc9cc81b86a676e2edb
Isabella Schepisi <schepisii2@gmail.com>
2025-03-26 21:05:20 -0500

    I am so confused

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import { createApp } from 'vue';
 import App from './App.vue';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
@@ -5,7 +6,7 @@ import { faArrowRight, faSeedling } from '@fortawesome/free-solid-svg-icons';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
 
-const app = Vue.createApp(App);
+const app = createApp(App);
 
 library.add(faArrowRight);
 library.add(faSeedling);
