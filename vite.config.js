294cf248cab13b938e24f28b2b55b980ff061105
Isabella Schepisi <schepisii2@gmail.com>
2025-03-26 20:38:29 -0500

    More deploy attempts

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,18 @@
+import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
 
 export default defineConfig({
-  plugins: [vue()],
-  test: {
-    globals: true,
-    environment: "jsdom",
-    },
+	base: '/plant-quiz/',
+	plugins: [vue(), vueJsx()],
+	resolve: {
+		alias: {
+			'@': fileURLToPath(new URL('./src', import.meta.url)),
+		},
+	},
+	test: {
+		globals: true,
+		environment: 'jsdom',
+	},
 });
